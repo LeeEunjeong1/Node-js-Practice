dcce6a4e6727ec564264a97f530e53ccaeb09c07
LeeEunjeong1 <tbig1019@gmail.com>
2021-05-28 10:25:03 +0900

    Server Practice

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ const server = app.listen(3000, ()=>{
     console.log('Start Server : localhost:3000');
 });
 
+app.set('views',__dirname+'/views'); //dirname 현재 디렉토리 의미, 거기의 views폴더 의미
+app.set('view engine',"ejs") //불러올 엔진 선언, html과 자바스크립트와 같이 쓰게 해주는 템플릿
+app.engine('html',require('ejs').renderFile);
+
 app.get('/',function(req,res) { //하나의 라우터
-    res.send('hello world')
+    res.render('index.html')
 })
 
 app.get('/about', function (req,res){
-    res.send('about page')
+    res.render('about.html')
 })
